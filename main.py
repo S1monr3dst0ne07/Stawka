3ddab3dbfd0e8b4654cdcd52dc69ec4212a9ca98
S1mon <jaforsi.kozak@gmail.com>
2025-09-06 22:16:19 +0200

    implemented reddit scrapper

--- main.py
+++ main.py
@@ -1,43 +1,58 @@
 import praw
 import json
 import argparse
+import sqlite3
 
 parser = argparse.ArgumentParser(
     prog='Stawka (/stɔuka/, cute version of stalker)',
     description='Collections, categorizes and stalkes, posts, projects and people related to programming languages development',
-    epilig="just a bit silly :3"
+    epilog="just a bit silly :3"
 )
 parser.add_argument("--database", action="store", default="main.db", help="database file path")
 parser.add_argument("--creds", action="store", default="creds.json", help="credentials file path")
 args = parser.parse_args()
 
 
+db = sqlite3.connect(args.database)
 
 
-with open(args.creds, 'r') as f:
-    cred = json.load(f)
-
-reddit = praw.Reddit(**cred)
-
-data = {}
 
 
-# Example: Get the 5 hottest posts from a subreddit
-s = reddit.subreddit("ProgrammingLanguages")
-for mode in (s.hot, s.new, s.rising):
-    for post in mode(limit = 1000):
-        if post.id in data:
-            continue
-
-        data[post.id] = {
-            "title" : post.title,
-            "score" : post.score,
-            "url"   : post.url,
-            "text"  : post.selftext,
-        }
 
 
-with open('data/posts.json', 'w') as f:
-    json.dump(data, f, indent=2)
+def update_reddit_posts(subreddit='ProgrammingLanguages'):
+    with open(args.creds, 'r') as f:
+        cred = json.load(f)
 
+    reddit = praw.Reddit(**cred)
+
+
+    cur = db.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS reddit (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        reddit_id TEXT UNIQUE,
+        title TEXT,
+        score INTEGER,
+        url TEXT,
+        content TEXT,
+        subreddit TEXT
+    );
+    """)
+
+
+    s = reddit.subreddit(subreddit)
+    for mode in (s.hot, s.new, s.rising):
+        for post in mode(limit = 5):
+            cur.execute("""
+                INSERT OR IGNORE INTO reddit (reddit_id, title, score, url, content, subreddit)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """, (post.id, post.title, post.score, post.url, post.selftext, subreddit)
+            )
+
+    db.commit()
+
+
+update_reddit_posts()
+db.close()
 
