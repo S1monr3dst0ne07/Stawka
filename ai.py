52e89aa42cb5edf58579ba936323b422a728554b
S1mon <jaforsi.kozak@gmail.com>
2025-09-11 00:32:18 +0200

    implemented repl loop for model

--- ai.py
+++ ai.py
@@ -69,14 +69,92 @@ def generate():
         for role, content in cur.fetchall()
     ]
 
-    print(messages)
+    result = client.chat.completions.create(
+        model="mistral",
+        messages = messages,
+        temperature=0.7,       
+        max_tokens=512,        
+        top_p=0.8,             
+        frequency_penalty=1.1, 
+        presence_penalty=0.5
+    )
+
+    response = result.choices[0].message.content
+    insert_message(response, "assistant")
+
+    return response
+
+
+def insert_schema():
+    insert_message("""
+    You are a function-calling AI.
+    You will always respond with a function call in JSON format.
+    You will ALWAYS respond in JSON.
+
+    Function call format: (what you'll be responding with)
+    {
+        "function": "<name>",
+        "arguments": { ... }
+    }
+
+
+    Available Functions:
+    - add(a: int, b: int)
+        Takes two integers, computes their sum.
+    - done(answer: string)
+        To be called when answer to user prompt has been found.
+
+
+    """, "system")
+
+def insert_system_message(msg):
+    insert_message(msg, "system")
+
+
+insert_message("What is 1 + 2 + 3?", "user")
+
+running = True
+def done(answer):
+    global running
+    print(answer)
+
+    running = False
+
+funcs = {
+    "add": lambda a, b: a + b,
+    "done": done,
+}
+
+while running:
+    resp_txt = generate()
+    print(resp_txt)
+
+    try:
+        resp = json.loads(resp_txt)
+        print(reps)
+
+        fn = resp["function"]
+        args = resp["arguments"]
+
+    except:
+        insert_system_message("Error: You have responsed with a malformed response, please adhere to the provided schema.")
+        insert_schema()
+        continue
+
+
+    if fn not in funcs:
+        insert_system_message("Error: You have responsed with a call to '{fn}', which is a function that does NOT exist.")
+        continue
+
+    try:
+        funcs[fn](**args)
+    except:
+        insert_system_message("Error: You have responsed with a call to '{fn}' while not providing the correct parameters for such a call.")
 
 
 
-insert_message("You are a function-calling AI.", "system")
-insert_message("What is 7 + 11?", "user")
-generate()
     
+db.close()
 
 
 
